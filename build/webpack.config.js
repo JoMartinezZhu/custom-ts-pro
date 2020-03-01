b1c7a8a76296425b95dad215a2b7ad655b372e43
zhushuangjie <henry.zhushuangjie@outlook.com>
2020-03-01 19:22:10 +0800

    config: update lint files

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -13,35 +13,35 @@ const isDevMode = process.env.NODE_ENV === 'development';
  * @type{import('webpack').Configuration}
  */
 module.exports = {
-  mode: process.env.NODE_ENV,
-  entry: resolve('src/index.tsx'),
-  output: {
-    path: resolve('dist'),
-    filename: isDevMode ? '[name].js' : '[name].[contenthash].js',
-    chunkFilename: isDevMode ? '[name].js' : '[name].[contenthash].js',
-  },
-  resolve: {
-    extensions: ['.js', '.jsx', '.ts', '.tsx'],
-    alias: {
-      '@pages': resolve('src/pages'),
-      '@components': resolve('src/components'),
-      '@store': resolve('src/store'),
-      '@services': resolve('src/services'),
+    mode: process.env.NODE_ENV,
+    entry: resolve('src/index.tsx'),
+    output: {
+        path: resolve('dist'),
+        filename: isDevMode ? '[name].js' : '[name].[contenthash].js',
+        chunkFilename: isDevMode ? '[name].js' : '[name].[contenthash].js'
     },
-  },
-  plugins,
-  optimization: isDevMode ? {} : optimization,
-  module: {
-    rules: [...tsRules, ...styleRules, ...fileRules],
-  },
-  devtool: isDevMode ? undefined : 'source-map', // TODO:分析source-map的选择
-  devServer: {
-    port: 8081,
-    disableHostCheck: true,
-    historyApiFallback: true, // 单页应用中当react的路由模式是BrowserRouter，historyApiFallback要设置为true,会在找不到页面的情况下跳转回index.html
-    host: '0.0.0.0',
-    after: function() {
-      openBrowser(`http://localhost:8081`);
+    resolve: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        alias: {
+            '@pages': resolve('src/pages'),
+            '@components': resolve('src/components'),
+            '@store': resolve('src/store'),
+            '@services': resolve('src/services')
+        }
     },
-  },
+    plugins,
+    optimization: isDevMode ? {} : optimization,
+    module: {
+        rules: [...tsRules, ...styleRules, ...fileRules]
+    },
+    devtool: isDevMode ? undefined : 'source-map', // TODO:分析source-map的选择
+    devServer: {
+        port: 8081,
+        disableHostCheck: true,
+        historyApiFallback: true, // 单页应用中当react的路由模式是BrowserRouter，historyApiFallback要设置为true,会在找不到页面的情况下跳转回index.html
+        host: '0.0.0.0',
+        after: function() {
+            openBrowser(`http://localhost:8081`);
+        }
+    }
 };
--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,19 @@
+/**
+ * @doc:commitlint.doc.png
+ * https://github.com/commitizen/cz-cli
+ * // feat:             feature, 表示新增了一个新功能
+ * fix:              表示这是一次bug fix
+ * improvement:      对现有功能的完善
+ * docs:             仅仅只是改变了文档类的文件，如readme.txt
+ * revert:           表示对以前的commit的撤回
+ * style:            只改变代码外观不改变代码功能 (如代码空格, 代码格式化, 行末冒号, 等等)
+ * refactor:         表示这是一次代码重构
+ * perf:             性能优化  *取自单词performance
+ * test:             增加或改变了测试代码
+ * chore:            其他不改变src或者test这两个文件夹的东西的一个commit
+ * build/config:            改变了构建配置类的文件，比如package.json增加打包命令，修改webpack的配置，修改.babelrc
+ * ci:               ci指的是持续集成。那么这里的意思是这是个改变CI配置的commit，比如Travis
+ */
 module.exports = {
     extends: ['@commitlint/config-conventional'],
     rules: {
