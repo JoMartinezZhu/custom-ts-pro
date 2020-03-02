7c3c799611e518a58e5901ccbcec88721ca18355
zhushuangjie <henry.zhushuangjie@outlook.com>
2020-03-02 15:48:20 +0800

    style: remove no use code

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,26 +1,10 @@
 import { parse } from 'querystring';
 import pathRegexp from 'path-to-regexp';
-import { Route } from '@/models/connect';
+import { Route } from '@models/connect';
 
 const reg = /(((^https?:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+(?::\d+)?|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)$/;
 
 export const isUrl = (path: string): boolean => reg.test(path);
-
-export const isAntDesignPro = (): boolean => {
-    if (ANT_DESIGN_PRO_ONLY_DO_NOT_USE_IN_YOUR_PRODUCTION === 'site') {
-        return true;
-    }
-    return window.location.hostname === 'preview.pro.ant.design';
-};
-
-// 给官方演示站点用，用于关闭真实开发环境不需要使用的特性
-export const isAntDesignProOrDev = (): boolean => {
-    const { NODE_ENV } = process.env;
-    if (NODE_ENV === 'development') {
-        return true;
-    }
-    return isAntDesignPro();
-};
 
 export const getPageQuery = () => parse(window.location.href.split('?')[1]);
 
@@ -32,7 +16,7 @@ export const getPageQuery = () => parse(window.location.href.split('?')[1]);
 export const getAuthorityFromRouter = <T extends Route>(router: T[] = [], pathname: string): T | undefined => {
     const authority = router.find(
         ({ routes, path = '/' }) =>
-            (path && pathRegexp(path).exec(pathname)) || (routes && getAuthorityFromRouter(routes, pathname))
+            (path && (pathRegexp(path) as any).exec(pathname)) || (routes && getAuthorityFromRouter(routes, pathname))
     );
     if (authority) return authority;
     return undefined;
